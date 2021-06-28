a75efbd4858f1e66ad397ff8bc81b51315ee2356
varshid-android <shekhatvarshid5145@gmail.com>
2021-06-28 14:19:13 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,44 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+
+
+const App= () => {
+  
+
+
+  return (
+    <SafeAreaView style={{flex:1}}>
+
+    </SafeAreaView>
+  );
+};
+
+
+
+export default App;
